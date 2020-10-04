45d0a9d554da9968b4123f1dc3885caf05bdb70a
feri muhibudin <38092606+ferimuhibudin@users.noreply.github.com>
2020-10-04 20:07:41 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,25 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Bank Sampah') }}</title>
+    
+    
+    <link rel="apple-touch-icon" sizes="57x57" href="https://img.bsdrajat.nix.id/icon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="https://img.bsdrajat.nix.id/icon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="https://img.bsdrajat.nix.id/icon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="https://img.bsdrajat.nix.id/icon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="https://img.bsdrajat.nix.id/icon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="https://img.bsdrajat.nix.id/icon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="https://img.bsdrajat.nix.id/icon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="https://img.bsdrajat.nix.id/icon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="https://img.bsdrajat.nix.id/icon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="https://img.bsdrajat.nix.id/icon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://img.bsdrajat.nix.id/icon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="https://img.bsdrajat.nix.id/icon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://img.bsdrajat.nix.id/icon/favicon-16x16.png">
+<link rel="manifest" href="https://img.bsdrajat.nix.id/icon/manifest.json">
+<meta name="msapplication-TileColor" content="green">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="green">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
